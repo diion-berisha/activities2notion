373b1fddff216ca57bcffab8c1ddfabf546e7aa6
Dion Berisha <diionberisha@gmail.com>
2024-12-27 23:45:58 +0100

    Added utils for coverting time, handeling properties and extracting from a json list

---
+++ src/utils/extract.py
@@ -0,0 +1,20 @@
+import jmespath
+
+
+def jmes_path(pattern, data, default=None):
+    """
+    Extracts data from a JSON object using a JMESPath pattern.
+
+    Args:
+        pattern (str): The JMESPath pattern to use.
+        data (dict): The JSON object to extract data from.
+        default (any, optional):
+        The default value to return if the pattern is not found. Defaults to None.
+
+    Returns:
+        any: The extracted data, or the default value if the pattern is not found.
+    """
+    result = jmespath.search(pattern, data)
+    if result is None and default is not None:
+        return default
+    return result
---
+++ src/utils/properties.py
@@ -0,0 +1,28 @@
+from utils.coverters import convert_seconds
+from utils.extract import jmes_path
+
+
+def define_properties(activity_data):
+    """
+    Defines the properties for a new activity entry.
+
+    Args:
+        activity_data (dict): The activity data to define the properties for.
+
+    Returns:
+        dict: The properties for the activity entry.
+    """
+    time = convert_seconds(jmes_path("time", activity_data))
+    return {
+        "Name": {"title": [{"text": {"content": jmes_path("name", activity_data)}}]},
+        "Activity ID": {"number": jmes_path("id", activity_data)},
+        "Distance (m)": {"number": jmes_path("distance", activity_data)},
+        "Moving Time": {"rich_text": [{"text": {"content": f"{time}"}}]},
+        "Average Heart Rate": {
+            "number": jmes_path("average_heart_rate", activity_data, 0)
+        },
+        "Max Heart Rate": {"number": jmes_path("max_heart_rate", activity_data, 0)},
+        "Average Cadence": {"number": jmes_path("average_cadence", activity_data, 0)},
+        "Type": {"select": {"name": jmes_path("type", activity_data)}},
+        "Date": {"date": {"start": jmes_path("start_date", activity_data)}},
+    }
